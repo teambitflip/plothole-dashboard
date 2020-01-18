09cc014d2c583ebef2a9ccd590ecfa9c929bc333
Priyansi <priyanhiman@gmail.com>
2020-01-18 18:07:56 +0000

    Update Severity2Map.js

--- src/components/Severity2Map.js
+++ src/components/Severity2Map.js
@@ -1,6 +1,18 @@
-import React, { Component } from "react";
-import firebase from "../firebase";
+import React, { Component, useState } from "react";
+import {
+  Collapse,
+  Button,
+  CardBody,
+  Card,
+  Row,
+  Col,
+  Container,
+  Modal,
+  ModalBody,
+  ModalHeader
+} from "reactstrap";
 import { compose } from "recompose";
+import firebase from "../firebase";
 import "./Map.css";
 import {
   withScriptjs,
@@ -9,7 +21,79 @@ import {
   Marker,
   InfoWindow
 } from "react-google-maps";
-
+const API_KEY = "AIzaSyArr8VXow5emvehdROfhZ7YcItqSBBNYbQ";
+const ListMenu = props => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => setModal(!modal);
+  return (
+    <div>
+      <Button
+        color="danger"
+        onClick={toggle}
+        style={{ marginBottom: "0.25rem", marginTop: "0.5rem", width: "100%" }}
+        block
+      >
+        ID : {props.value.id}
+      </Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+            <Container fluid>
+              <Row>
+                <Col>
+                  <img
+                    src={props.value.img_link}
+                    style={{
+                      width: "150px",
+                      height: "150px"
+                    }}
+                  ></img>
+                </Col>
+                <Col>
+                  <h6>Latitude : {props.value.latitude}</h6>
+                  <h6>Longitude : {props.value.longitude}</h6>
+                  <h6>Priority : {props.value.priority}</h6>
+                  <h6>Action Taken : {props.value.action_taken}</h6>
+                  <br></br>
+                  <Button outline color="danger" onClick={toggleModal}>
+                    Image Link
+                  </Button>
+                  <Modal isOpen={modal} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>
+                      Coordinates : {props.value.latitude},
+                      {props.value.longitude}
+                    </ModalHeader>
+                    <ModalBody>
+                      <img
+                        src={props.value.img_link}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          padding: "1rem"
+                        }}
+                      ></img>
+                    </ModalBody>
+                  </Modal>
+                </Col>
+                <Col>
+                  <iframe
+                    width="100%"
+                    frameborder="0"
+                    style={{ border: "0" }}
+                    src={`https://www.google.com/maps/embed/v1/place?q=${props.value.latitude},${props.value.longitude}&key=${API_KEY}&zoom=18`}
+                    allowfullscreen
+                  ></iframe>
+                </Col>
+              </Row>
+            </Container>
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+  );
+};
 const MapWithAMarker = compose(
   withScriptjs,
   withGoogleMap
@@ -20,11 +104,12 @@ const MapWithAMarker = compose(
       defaultCenter={{ lat: 20.3546207, lng: 85.8206245 }}
     >
       {props.markers.map(marker => {
-        const onClick = props.onClick.bind(this, marker);
+        const onMouseOver = props.onMouseOver.bind(this, marker);
+
         return (
           <Marker
             key={marker.id}
-            onClick={onClick}
+            onMouseOver={onMouseOver}
             position={{
               lat: parseFloat(marker.latitude),
               lng: parseFloat(marker.longitude)
@@ -32,29 +117,8 @@ const MapWithAMarker = compose(
           >
             {props.selectedMarker === marker && (
               <InfoWindow>
-                <div className="infoWindowStyle">
-                  <div className="card" style={{ maxWidth: "100%" }}>
-                    <img
-                      src={marker.img_link}
-                      style={{
-                        width: "80%",
-                        paddingTop: "1rem",
-                        alignSelf: "center"
-                      }}
-                    ></img>
-
-                    <div className="card-body" style={{ textAlign: "left" }}>
-                      <h6 className="card-title">
-                        Validity : {marker.validity}%
-                      </h6>
-                      <h6 className="card-title">
-                        Priority : {marker.priority}
-                      </h6>
-                      <h6 className="card-title">
-                        Action Taken : {marker.action_taken}
-                      </h6>
-                    </div>
-                  </div>
+                <div>
+                  <h6>ID : {marker.id}</h6>
                 </div>
               </InfoWindow>
             )}
@@ -65,7 +129,7 @@ const MapWithAMarker = compose(
   );
 });
 
-export default class Severity2Map extends Component {
+export default class Severity1Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -82,6 +146,7 @@ export default class Severity2Map extends Component {
       let assignLatitude = [];
       let assignLongitude = [];
       let assignAction = [];
+      let a = 1;
       for (let item in places) {
         latLng = places[item].gps_coordinates.split(",");
         assignLatitude = latLng[0];
@@ -96,15 +161,16 @@ export default class Severity2Map extends Component {
           places[item].severity === 2
         ) {
           potholes.push({
-            id: item,
-            latitude: assignLatitude,
-            longitude: assignLongitude,
+            id: a,
+            latitude: parseFloat(assignLatitude),
+            longitude: parseFloat(assignLongitude),
             severity: places[item].severity,
             validity: Math.floor(places[item].validity),
             img_link: places[item].img_link,
             action_taken: assignAction,
-            priority: "Medium to High"
+            priority: "Medium To High"
           });
+          a = a + 1;
         }
       }
       this.setState({
@@ -112,20 +178,30 @@ export default class Severity2Map extends Component {
       });
     });
   }
-  handleClick = (marker, event) => {
-    this.setState({ selectedMarker: marker });
+  handleMouseOver = marker => {
+    this.setState({
+      selectedMarker: marker
+    });
   };
+
   render() {
     return (
-      <MapWithAMarker
-        selectedMarker={this.state.selectedMarker}
-        markers={this.state.places}
-        onClick={this.handleClick}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHrKJe9b5FK5azAKfYo5MoH98-Lv9Q8ag&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `500px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <React.Fragment>
+        <MapWithAMarker
+          selectedMarker={this.state.selectedMarker}
+          markers={this.state.places}
+          onMouseOver={this.handleMouseOver}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHrKJe9b5FK5azAKfYo5MoH98-Lv9Q8ag&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `300px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+        <div>
+          {this.state.places.map(data => (
+            <ListMenu key={data.id} value={data} />
+          ))}
+        </div>
+      </React.Fragment>
     );
   }
 }
