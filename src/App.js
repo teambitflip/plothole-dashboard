9e1d3a9c20c83d710c33aa841f37459f03081a69
Priyansi <priyanhiman@gmail.com>
2020-01-30 16:15:18 +0000

    Accommodated Severity 0 images

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { useState } from "react";
+import logo from "./logo.png";
+import "./App.css";
 import Map from "./components/Map";
 import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-import logo from "./logo.png";
-import "./App.css";
-
+import Severity0Map from "./components/Severity0Map";
 import Severity1Map from "./components/Severity1Map";
 import Severity2Map from "./components/Severity2Map";
 import Severity3Map from "./components/Severity3Map";
-
-
 const openMap = props => {
   console.log("map", props.location.mapProps);
   return <Map />;
 };
-
+const openSeverity0Map = props => {
+  console.log("severity0map", props.location.severity0MapProps);
+  return <Severity0Map />;
+};
 const openSeverity1Map = props => {
   console.log("severity1map", props.location.severity1MapProps);
   return <Severity1Map />;
 };
-
 const openSeverity2Map = props => {
   console.log("severity2map", props.location.severity2MapProps);
   return <Severity2Map />;
 };
-
 const openSeverity3Map = props => {
   console.log("severity3map", props.location.severity3MapProps);
   return <Severity3Map />;
 };
-
-
 const NavBar = () => {
   const [collapsed, setCollapsed] = useState(true);
   const toggleNavbar = () => setCollapsed(!collapsed);
@@ -39,7 +35,7 @@ const NavBar = () => {
     <Router>
       <div>
         <Navbar color="p-3 bg-dark text-white" dark>
-          <img alt="logo" src={logo} className="img-responsive"></img>
+          <img src={logo} className="img-responsive"></img>
 
           <Link
             className="mr-auto"
@@ -56,6 +52,17 @@ const NavBar = () => {
           <Collapse isOpen={!collapsed} navbar>
             <Nav navbar>
               <NavItem>
+                <Link
+                  style={{ color: "white" }}
+                  to={{
+                    pathname: "/severity0map",
+                    severity0MapProps: { name: "Severity 0 map was opened" }
+                  }}
+                >
+                  Severity 0
+                </Link>
+              </NavItem>
+              <NavItem style={{ paddingTop: "0.5rem" }}>
                 <Link
                   style={{ color: "white" }}
                   to={{
@@ -92,6 +99,7 @@ const NavBar = () => {
           </Collapse>
         </Navbar>
         <Route path="/" exact component={openMap}></Route>
+        <Route path="/severity0map/" component={openSeverity0Map}></Route>
         <Route path="/severity1map/" component={openSeverity1Map}></Route>
         <Route path="/severity2map/" component={openSeverity2Map}></Route>
         <Route path="/severity3map/" component={openSeverity3Map}></Route>
