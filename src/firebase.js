ee2f06f6a0d6d8d3b7f763e010d4bbc9c3a8f736
Priyansi <priyanhiman@gmail.com>
2020-01-31 18:43:31 +0000

    removed minor warning

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase'
+import firebase from "firebase/app";
+import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAaTTJutZtbmlQS7O1XvJoauefLUAb2xG0",
-    authDomain: "bitflip-potholedetector.firebaseapp.com",
-    databaseURL: "https://bitflip-potholedetector.firebaseio.com",
-    projectId: "bitflip-potholedetector",
-    storageBucket: "bitflip-potholedetector.appspot.com",
-    messagingSenderId: "1044942737663",
-    appId: "1:1044942737663:web:29bad1229f91a4cb83bcca"
+  apiKey: "AIzaSyAaTTJutZtbmlQS7O1XvJoauefLUAb2xG0",
+  authDomain: "bitflip-potholedetector.firebaseapp.com",
+  databaseURL: "https://bitflip-potholedetector.firebaseio.com",
+  projectId: "bitflip-potholedetector",
+  storageBucket: "bitflip-potholedetector.appspot.com",
+  messagingSenderId: "1044942737663",
+  appId: "1:1044942737663:web:29bad1229f91a4cb83bcca"
 };
 
-let firebaseapp = firebase.initializeApp(firebaseConfig)
+let firebaseapp = firebase.initializeApp(firebaseConfig);
 
-
-export default firebaseapp
+export default firebaseapp;
