9331ad65b78789b63f3cd2dd76848c8714ad3d07
Priyansi <priyanhiman@gmail.com>
2020-01-31 18:42:39 +0000

    removed minor warnings

--- src/App.js
+++ src/App.js
@@ -8,24 +8,19 @@ import Severity0Map from "./components/Severity0Map";
 import Severity1Map from "./components/Severity1Map";
 import Severity2Map from "./components/Severity2Map";
 import Severity3Map from "./components/Severity3Map";
-const openMap = props => {
-  console.log("map", props.location.mapProps);
+const openMap = () => {
   return <Map />;
 };
-const openSeverity0Map = props => {
-  console.log("severity0map", props.location.severity0MapProps);
+const openSeverity0Map = () => {
   return <Severity0Map />;
 };
-const openSeverity1Map = props => {
-  console.log("severity1map", props.location.severity1MapProps);
+const openSeverity1Map = () => {
   return <Severity1Map />;
 };
-const openSeverity2Map = props => {
-  console.log("severity2map", props.location.severity2MapProps);
+const openSeverity2Map = () => {
   return <Severity2Map />;
 };
-const openSeverity3Map = props => {
-  console.log("severity3map", props.location.severity3MapProps);
+const openSeverity3Map = () => {
   return <Severity3Map />;
 };
 const NavBar = () => {
@@ -35,12 +30,9 @@ const NavBar = () => {
     <Router>
       <div>
         <Navbar color="p-3 bg-dark text-white" dark>
-          <img src={logo} className="img-responsive"></img>
+          <img alt="logo" src={logo} className="img-responsive"></img>
 
-          <Link
-            className="mr-auto"
-            to={{ pathname: "/", mapProps: { name: "Main map was opened" } }}
-          >
+          <Link className="mr-auto" to={{ pathname: "/" }}>
             <div>
               <h2 className="h2-style" style={{ color: "white" }}>
                 PlotHole - Team BitFlip
@@ -55,8 +47,7 @@ const NavBar = () => {
                 <Link
                   style={{ color: "white" }}
                   to={{
-                    pathname: "/severity0map",
-                    severity0MapProps: { name: "Severity 0 map was opened" }
+                    pathname: "/severity0map"
                   }}
                 >
                   Severity 0
@@ -66,8 +57,7 @@ const NavBar = () => {
                 <Link
                   style={{ color: "white" }}
                   to={{
-                    pathname: "/severity1map",
-                    severity1MapProps: { name: "Severity 1 map was opened" }
+                    pathname: "/severity1map"
                   }}
                 >
                   Severity 1
@@ -77,8 +67,7 @@ const NavBar = () => {
                 <Link
                   style={{ color: "white" }}
                   to={{
-                    pathname: "/severity2map",
-                    severity2MapProps: { name: "Severity 2 map was opened" }
+                    pathname: "/severity2map"
                   }}
                 >
                   Severity 2
@@ -88,8 +77,7 @@ const NavBar = () => {
                 <Link
                   style={{ color: "white" }}
                   to={{
-                    pathname: "/severity3map",
-                    severity3MapProps: { name: "Severity 3 map was opened" }
+                    pathname: "/severity3map"
                   }}
                 >
                   Severity 3
